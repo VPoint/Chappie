bc7669a2e4d36a44616f2350e5dad1c6c93c2be9
VPoint <estar.raji@gmail.com>
2019-08-07 22:56:19 -0400

    Started working on theming. Attempt to add plugin into admin menu (currently failing).

---
+++ apps/plugins/event/app/helpers/plugins/event_menu/main_helper.rb
@@ -0,0 +1,12 @@
+module class Plugins::EventMenu::AdminController < CamaleonCms::Apps::PluginsAdminController
+  def event_admin_before_load
+    # add menus
+    sub_items1 = [
+        {icon: "list", title: 'List of my Events', url: '#url_path_here'},
+        {icon: "plus", title: 'New Event', url: '#url_path_to_new_here'}
+    ]
+
+    # add menus after certain menu
+    admin_menu_insert_menu_after("content", "my_plugin_menu", {icon: 'briefcase', title: 'My Menu Group', url: "", items: sub_items1})
+
+  end
